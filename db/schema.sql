1eef2a07db643bb615ebc734204412a0c1346eec
Kevin M <kevin.mahone19@gmail.com>
2019-05-30 12:35:10 -0400

    party table set up

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS GrudgeBook_db;
+
+CREATE DATABASE GrudgeBook_db;
---
+++ models/party.js
@@ -0,0 +1,21 @@
+module.exports = function(sequelize, DataTypes){
+  var Party = sequelize.define("Party", {
+    party_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    party_lvl: {
+      type: DataTypes.INT,
+      defaultValue: 1,
+      allowNull: false,
+    }
+  },
+  {
+    timestamps: false
+  });
+
+  return Party;
+};
